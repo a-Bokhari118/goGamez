26e2ce6d4f575aa0b9404698bc106fc7c0829455
Slade Wilson <a.bokhari118@hotmail.com>
2020-12-04 01:36:02 +0300

    setting up middleware

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDom from 'react-dom';
 import App from './App';
 
 //Redux setup
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 ReactDom.render(
   <React.StrictMode>
     <Provider store={store}>
